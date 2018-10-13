5ebf8e9a59433abdc2ba87ddec5ce5d3558fad41
pqnotavel <raispbizerra@gmail.com>
2018-10-13 14:34:10 -0300

    Etapa 2

--- docs/js/init.js
+++
@@ -1,9 +0,0 @@
-(function($){
-  $(function(){
-
-    $('.sidenav').sidenav();
-    $('.slider').slider();
-    $('.carousel').carousel();
-    
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
